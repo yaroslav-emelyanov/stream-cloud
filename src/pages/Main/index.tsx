f7edbf3faa869d708071e9c3f2bf9ad2e710e01c
Yarolsav.Emelyanov <yaroslav.emelyanov@x5.ru>
2021-12-07 14:01:31 +0300

    fixed main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,52 +1,7 @@
 import React from 'react';
-import { useSearchParams } from 'react-router-dom';
-
-import MovieCard from '@components/MovieCard';
-import { CircularProgress } from '@mui/material';
-
-import { pagination, useMovies, useHasMorePages } from '@entities/movie';
-import { DialogTypes } from '@shared/constants';
-
-import { usePageGate } from './model';
-import { EndMessage, ProgressWrapper, InfiniteScroll } from './styles';
 
 const MainPage = () => {
-  const [, setSearchParams] = useSearchParams();
-  const hasMore = useHasMorePages();
-  const movies = useMovies();
-
-  usePageGate();
-
-  return (
-    <InfiniteScroll
-      dataLength={movies.length}
-      next={pagination.nextPage}
-      endMessage={
-        <EndMessage variant="body1" align="center">
-          Больше нет записей
-        </EndMessage>
-      }
-      loader={
-        <ProgressWrapper>
-          <CircularProgress />
-        </ProgressWrapper>
-      }
-      hasMore={hasMore}
-    >
-      {movies.map((movie) => (
-        <MovieCard
-          movie={movie}
-          onClick={() =>
-            setSearchParams({
-              dialog: DialogTypes.PREVIEW,
-              kinopoisk_id: movie.kinopoisk_id,
-            })
-          }
-          key={movie.id}
-        />
-      ))}
-    </InfiniteScroll>
-  );
+  return <div>Main page</div>;
 };
 
 export default MainPage;
